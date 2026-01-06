834af1531f6891d0c4e25b18d4f32c886380bfd5
slzslzl <zubikhin.slav@yandex.ru>
2026-01-06 18:02:48 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -34,8 +34,7 @@ module.exports = async (req, res) => {
              throw new Error("Тело запроса пустое");
         }
 
-        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
-
+        const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}';
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
